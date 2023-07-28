b971b4659531da88c7a583ee52c53c3d53111598
Brady L. Hurlburt <bhurlburt@salesforce.com>
2023-07-28 13:59:42 -0400

    More prerender issue

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,7 @@
 import type { LayoutServerLoad } from "./$types"
 
+export const prerender = false;
+
 export const load: LayoutServerLoad = async (event) => {
   return {
     session: await event.locals.getSession(),
