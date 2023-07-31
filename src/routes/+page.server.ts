7753975d07b578f078640767d96fe12a2d8335c7
Brady L. Hurlburt <bhurlburt@salesforce.com>
2023-07-31 15:37:07 -0400

    Force fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,17 @@
-import type { PageServerLoad, Actions } from "./$types";
+import type { PageServerLoad, Actions, User } from "./$types";
 import prisma from '$lib/prisma';
 
 export const load: PageServerLoad = async ({ parent }) => {
   const data = await parent()
-  const user = data.user;
+  let user = data.user;
   const session = data.session;
 
   // Not sure why this is also needed here
+  if (user == null) {
+    user = await prisma.user.findUnique({
+      where: { email: session.user.email }
+    })
+  }
   if (user == null) {
     try {
       const createUserResponse = await prisma.user.create({
@@ -16,7 +21,7 @@ export const load: PageServerLoad = async ({ parent }) => {
         }
       })
     } catch {
-      console.log("Error in page user create. What is going on.");
+      console.log("Error in page user create.");
     }
   }
 
